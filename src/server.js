6264c4acdd72eae6a5206d4130d05bfaf85cb719
bbubbush <bbubbush@gmail.com>
2023-01-14 21:28:44 +0900

    End of Frontend Setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
+import path from 'path';
 import express from "express";
+const __dirname = path.resolve();
 
 const app = express();
+app.set("view engine", "pug")
+app.set("views", __dirname + "/src/views")
+app.use("/public", express.static(__dirname + "/src/public"))
+//console.log(express) 
+app.get("/", (req, res) => res.render("home"))
+
 
 console.log('hello');
 
-app.listen(3000)
+app.listen(3000, () => console.log('Listen!'))
